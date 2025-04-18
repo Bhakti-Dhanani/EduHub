75184befb98871c2ce2d3ee5766d3c0301a5f918
Bhakti-Dhanani <bhagvatiben.wappnet@gmail.com>
2025-04-18 17:46:10 +0530

    athentication based on the user role

--- EduHub/config/plugins.ts
+++ EduHub/config/plugins.ts
@@ -1,5 +1,11 @@
+// E:/LMS/EduHub/config/plugins.js
 module.exports = ({ env }) => ({
     'users-permissions': {
       enabled: true,
+      config: {
+        jwt: {
+          expiresIn: '7d',
+        },
+      },
     },
   });
